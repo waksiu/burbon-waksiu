5408350da91608caaad9c58c4caf8e8d377f099f
waksiu <gentoo2x@gmail.com>
2018-02-28 15:26:02 +0100

    travis bash

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,13 @@
-curl --ftp-create-dirs -T static.html -u $FTP_USER:$FTP_PASSWORD ftp://s26.zenbox.pl/domains/capitaltemplate.com/public_html/templates/bourbon/demo/
+#!/bin/bash
+
+function deployFromTo () {
+	for f in $1; do
+		if [[ -f "$f" ]]; then
+			echo "${f}"
+			curl --ftp-create-dirs -T ${f} -u $FTP_USER:$FTP_PASSWORD "ftp://s26.zenbox.pl/$2"
+		fi
+	done
+}
+
+deployFromTo "*" "domains/capitaltemplate.com/public_html/templates/bourbon/demo/"
+deployFromTo "css/*" "domains/capitaltemplate.com/public_html/templates/bourbon/demo/css/"
