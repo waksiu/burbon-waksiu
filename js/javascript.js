d48938b3f0c3de43a8489f49194467bedb397940
waksiu <gentoo2x@gmail.com>
2018-03-08 22:29:51 +0100

    background

--- js/javascript.js
+++ js/javascript.js
@@ -200,9 +200,9 @@ $(document).ready(function( $ ) {
     }
 
     // preloader
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({'overflow':'visible'});
+    // $('#status').fadeOut(); // will first fade out the loading animation
+    // $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    // $('body').delay(350).css({'overflow':'visible'});
 
     if( $('#counterDown').length ) {
         $("#counterDown").countdown(
